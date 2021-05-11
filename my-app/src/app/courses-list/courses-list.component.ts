961f1655b389695dd78897527104b2eef1b4253c
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-05-11 17:54:18 +0530

    Export As property usage

--- my-app/src/app/courses-list/courses-list.component.ts
+++ my-app/src/app/courses-list/courses-list.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.css'],
+  exportAs: 'courselist'
 })
 export class CoursesListComponent implements OnInit {
   constructor() {}
@@ -12,8 +13,22 @@ export class CoursesListComponent implements OnInit {
     this.courses = [
       { courseID: 1, courseName: 'Angular' },
       { courseID: 2, courseName: 'Java' },
-      { courseID: 2, courseName: 'Spring' },
+      { courseID: 3, courseName: 'Spring' },
       { courseID: 4, courseName: 'SpringBoot' },
     ];
+  }
+
+  course: any[];
+
+  changeCourse(courseName: string)
+  {
+    this.course=[];
+    for(let i=0;i<this.courses.length;i++)
+    {
+      if(this.courses[i].courseName===courseName)
+      {
+        this.course.push(this.courses[i]);
+      }
+    }
   }
 }
